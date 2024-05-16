7d0a04470118ce134d5dc56adb1b667912bc7db9
Abass Koyang <abasskoyang12345@gmail.com>
2024-05-16 22:40:12 +0100

    "Updated Model component with accessibility attributes and styling changes for iPhone image and color selector buttons."

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -116,29 +116,64 @@ const Model = () => {
 
             <div id='con' className="flex flex-col items-center mt-10 relative">
                 <div className="w-full h-[75vh] md:h-[90vh] overflow-hidden flex-center">
-                    <img className='transition-all duration-300 ease-in-out' src={iphoneImg} alt="Iphone image" />
+                    <img className={`${iphoneColor === 'all-colors' ? 'w-[329px]' : 'w-[224px]'} md:w-auto transition-all duration-300 ease-in-out`} src={iphoneImg} alt="Iphone image" />
                 </div>
                 {/* <button id='test' className='sticky bottom-10 text-5xl text-red-500 translate-y-[100px] opacity-0'>TestButton</button> */}
                 <div id='control-buttons-con' className="mt-16 flex-center flex-col sticky bottom-10 translate-y-[100px]">
-                    <p id='desc' className='text-xs font-semibold text-white transition-all duration-300 ease-in-out opacity-0'>{iphoneDescription}</p>
-                    <div id='control-buttons-wrapper' className="scale-0 py-2.5 px-5 bg-gray-300 backdrop-blur rounded-full flex-center gap-3 mt-3.5 w-fit">
-                        <button onClick={() => setIphoneColor('all-colors')} className={`${iphoneColor === 'all-colors' ? 'p-1 bg-white border-2 border-blue' : 'p-0 '} transition-all duration-300 ease-in-out rounded-full`}>
+                    <p id='desc' className='bg-[#0000004D] rounded-[5px] px-1.5 py-1 backdrop-blur max-w-xs text-center  text-pretty text-xs font-semibold text-white transition-all duration-300 ease-in-out opacity-0'>{iphoneDescription}</p>
+                    <div aria-label='Iphone color selector container' id='control-buttons-wrapper' className="scale-0 py-2.5 px-5 bg-gray-300 backdrop-blur rounded-full flex-center gap-3 mt-3.5 w-fit">
+                        <button 
+                        aria-label='Click to show 6.1” iPhone 15 Pro1 in four colors' 
+                        aria-haspopup={true} 
+                        aria-expanded={iphoneColor === 'all-colors'} 
+                        aria-selected={iphoneColor === 'all-colors'} 
+                        onClick={() => setIphoneColor('all-colors')} 
+                        className={`${iphoneColor === 'all-colors' ? 'p-1 bg-white border-2 border-blue' : 'p-0 '} transition-all duration-300 ease-in-out rounded-full`}>
                             <div className={`model_color_buttons bg-gradient-to-r from-[#8F8A81] via-[#202630] to-[#242526] ${iphoneColor === 'all-colors' ? 'border-2 border-gray-300' : ''} transition-all duration-300 ease-in-out`}></div>
                         </button>
 
-                        <button onClick={() => setIphoneColor('natural-titanium')} className={`${iphoneColor === 'natural-titanium' ? 'p-1 bg-white border-2 border-blue' : 'p-0 '} transition-all duration-300 ease-in-out rounded-full`}>
-                        <div className={`model_color_buttons bg-[#8F8A81] ${iphoneColor === 'natural-titanium' ? 'border-2 border-gray-300' : ''} transition-all duration-300 ease-in-out`}></div>
+                        <button 
+                        aria-label='Click to show 6.7” iPhone 15 Pro Max1 and
+                        6.1” iPhone 15 Pro1 in Natural Titanium' 
+                        aria-haspopup={true} 
+                        aria-expanded={iphoneColor === 'natural-titanium'} 
+                        aria-selected={iphoneColor === 'natural-titanium'} 
+                        onClick={() => setIphoneColor('natural-titanium')} 
+                        className={`${iphoneColor === 'natural-titanium' ? 'p-1 bg-white border-2 border-blue' : 'p-0 '} transition-all duration-300 ease-in-out rounded-full`}>
+                        <div 
+                        className={`model_color_buttons bg-[#8F8A81] ${iphoneColor === 'natural-titanium' ? 'border-2 border-gray-300' : ''} transition-all duration-300 ease-in-out`}></div>
                         </button>
                         
-                        <button onClick={() => setIphoneColor('blue-titanium')} className={`${iphoneColor === 'blue-titanium' ? 'p-1 bg-white border-2 border-blue' : 'p-0 '} transition-all duration-300 ease-in-out rounded-full`}>
+                        <button
+                        aria-label='Click to show 6.7” iPhone 15 Pro Max1 and
+                        6.1” iPhone 15 Pro1 in Blue Titanium' 
+                        aria-haspopup={true} 
+                        aria-expanded={iphoneColor === 'blue-titanium'} 
+                        aria-selected={iphoneColor === 'blue-titanium'} 
+                        onClick={() => setIphoneColor('blue-titanium')} 
+                        className={`${iphoneColor === 'blue-titanium' ? 'p-1 bg-white border-2 border-blue' : 'p-0 '} transition-all duration-300 ease-in-out rounded-full`}>
                         <div className={`model_color_buttons bg-[#202630] ${iphoneColor === 'blue-titanium' ? 'border-2 border-gray-300' : ''} transition-all duration-300 ease-in-out`}></div>
                         </button>
 
-                        <button onClick={() => setIphoneColor('white-titanium')} className={`${iphoneColor === 'white-titanium' ? 'p-1 bg-white border-2 border-blue' : 'p-0 '} transition-all duration-300 ease-in-out rounded-full`}>
+                        <button 
+                        aria-label='Click to show 6.7” iPhone 15 Pro Max1 and
+                        6.1” iPhone 15 Pro1 in White Titanium' 
+                        aria-haspopup={true} 
+                        aria-expanded={iphoneColor === 'white-titanium'} 
+                        aria-selected={iphoneColor === 'white-titanium'}
+                        onClick={() => setIphoneColor('white-titanium')} 
+                        className={`${iphoneColor === 'white-titanium' ? 'p-1 bg-white border-2 border-blue' : 'p-0 '} transition-all duration-300 ease-in-out rounded-full`}>
                         <div className={`model_color_buttons bg-[#C9C8C3] ${iphoneColor === 'white-titanium' ? 'border-2 border-gray-300' : ''} transition-all duration-300 ease-in-out`}></div>
                         </button>
 
-                        <button onClick={() => setIphoneColor('black-titanium')} className={`${iphoneColor === 'black-titanium' ? 'p-1 bg-white border-2 border-blue' : 'p-0 '} transition-all duration-300 ease-in-out rounded-full`}>
+                        <button
+                        aria-label='Click to show 6.7” iPhone 15 Pro Max1 and
+                        6.1” iPhone 15 Pro1 in Black Titanium' 
+                        aria-haspopup={true} 
+                        aria-expanded={iphoneColor === 'black-titanium'} 
+                        aria-selected={iphoneColor === 'black-titanium'} 
+                        onClick={() => setIphoneColor('black-titanium')} 
+                        className={`${iphoneColor === 'black-titanium' ? 'p-1 bg-white border-2 border-blue' : 'p-0 '} transition-all duration-300 ease-in-out rounded-full`}>
                         <div className={`model_color_buttons bg-[#242526] ${iphoneColor === 'black-titanium' ? 'border-2 border-gray-300' : ''} transition-all duration-300 ease-in-out`}></div>
                         </button>
                     </div>
