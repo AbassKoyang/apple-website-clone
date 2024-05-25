6d74f36a63d04bc991288186d8081e10626b0d48
Abass Koyang <abasskoyang12345@gmail.com>
2024-05-25 23:59:19 +0100

    "Update FullStoryModal component and add typography class to index.css"

--- src/components/FullStoryModal.jsx
+++ src/components/FullStoryModal.jsx
@@ -16,7 +16,7 @@ const FullStoryModal = ({visible, handleClick}) => {
                 </button>
             </div>
             <div className="w-full bg-black">
-              <h1 className='ml-20 md:ml-60 max-w-[200px] md:max-w-max text-[35px] md:text-[60px] text-[#F5F5F7] font-semibold leading-[1.1] md:leading-[1] tracking-[0em] md:tracking-[-0.015em] pt-[60px]'>iPhone. <br />Forged in titanium.</h1>
+              <h1 className='ml-20 lg:ml-60 max-w-[200px] md:max-w-max text-[35px] md:text-[60px] text-[#F5F5F7] font-semibold leading-[1.1] md:leading-[1] tracking-[0em] md:tracking-[-0.015em] pt-[60px]'>iPhone. <br />Forged in titanium.</h1>
               <div className="w-full h-full bg-black mt-14 mb-[120px]">
                 <video className='pointer-events-none' preload='auto' autoPlay playsInline muted key={fullStoryModalVideo}>
                     <source src={fullStoryModalVideo} type='video/mp4'/>
@@ -24,9 +24,19 @@ const FullStoryModal = ({visible, handleClick}) => {
               </div>
             </div>
 
-            <div className="w-full bg-zinc pt-[120px]">
-              <div className="w-full max-w-[]">
+            <div className="w-full flex-center bg-zinc pt-[120px]">
+              <div className="w-[87.5%] md:w-full max-w-[312px] md:max-w-[720px] lg:max-w-[960px] grid grid-cols-2  md:gap-[7%]">
+                <div className="col-span-1">
+                  <div className="w-full h-[500px] bg-blue">
+                  </div>
+                  <p className='typography-site-body-fullstory-modal pb-[50px] md:pb-0 mt-[38px] lg:ml-20'>The <strong className='text-[#f5f5f7] font-semibold'>beautiful, fine‑brushed finish</strong> on the titanium bands is achieved through precision machining, sanding, brushing, and blasting.</p>
+                </div>
+                <div className="col-span-1">
+                  <div className="w-full h-[500px] bg-blue">
 
+                  </div>
+                  <p className='typography-site-body-fullstory-modal pb-[50px] md:pb-0 mt-[38px] lg:ml-20'>New contoured edges and the thinnest borders ever on iPhone make it even <strong className='text-[#f5f5f7] font-semibold'>more comfortable to hold</strong> in your hand.</p>
+                </div>
               </div>
             </div>
         </div>
