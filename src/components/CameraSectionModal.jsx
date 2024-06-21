8d1cd87873f96fe65c2afaac7139a1ec618bd2e9
Abass Koyang <abasskoyang12345@gmail.com>
2024-06-21 17:15:21 +0100

    "Added cameraSectionModalImg7 import, updated GSAP animations, and added new section with image and text animations"

--- src/components/CameraSectionModal.jsx
+++ src/components/CameraSectionModal.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { RiCloseFill } from 'react-icons/ri'
-import { cameraSectionModalImg1, cameraSectionModalImg2, cameraSectionModalImg3, cameraSectionModalImg4, cameraSectionModalImg5, cameraSectionModalImg6 } from '../utils'
+import { cameraSectionModalImg1, cameraSectionModalImg2, cameraSectionModalImg3, cameraSectionModalImg4, cameraSectionModalImg5, cameraSectionModalImg6, cameraSectionModalImg7 } from '../utils'
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
 
@@ -17,7 +17,7 @@ const CameraSectionModal = ({visible, handleClick}) => {
           end: 'bottom 0%',
           scroller: '#camera-section-modal',
       }
-  })
+    })
     gsap.to('#camera-section-modal-text2', {
       y: 0,
       opacity: 1,
@@ -28,7 +28,7 @@ const CameraSectionModal = ({visible, handleClick}) => {
           end: 'bottom 0%',
           scroller: '#camera-section-modal',
       }
-  })
+    })
     gsap.to('#camera-section-modal-text3', {
       y: 0,
       opacity: 1,
@@ -39,7 +39,7 @@ const CameraSectionModal = ({visible, handleClick}) => {
           end: 'bottom 0%',
           scroller: '#camera-section-modal',
       }
-  })
+   })
     gsap.to('#camera-section-modal-text4', {
       y: 0,
       opacity: 1,
@@ -49,8 +49,50 @@ const CameraSectionModal = ({visible, handleClick}) => {
           start: 'top 70%',
           end: 'bottom 0%',
           scroller: '#camera-section-modal',
-      }
-  })
+    }})
+    gsap.to('#camera-section-modal-text5', {
+      y: 0,
+      opacity: 1,
+      scrollTrigger: {
+          trigger: '#camera-section-modal-text5',
+          toggleActions: "play play play reverse",
+          start: 'top 70%',
+          end: 'bottom 0%',
+          scroller: '#camera-section-modal',
+    }})
+    gsap.to('#camera-section-modal-text6', {
+      y: 0,
+      opacity: 1,
+      scrollTrigger: {
+          trigger: '#camera-section-modal-text6',
+          toggleActions: "play play play reverse",
+          start: 'top 70%',
+          end: 'bottom 0%',
+          scroller: '#camera-section-modal',
+    }})
+    gsap.to('#camera-section-modal-text7', {
+      y: 0,
+      opacity: 1,
+      scrollTrigger: {
+          trigger: '#camera-section-modal-text7',
+          toggleActions: "play play play reverse",
+          start: 'top 70%',
+          end: 'bottom 0%',
+          scroller: '#camera-section-modal',
+    }})
+
+
+
+    // Image animations
+    gsap.to('#camera-section-modal-image7', {
+      opacity: 1,
+      scrollTrigger: {
+          trigger: '#camera-section-image7',
+          toggleActions: "play play play reverse",
+          start: 'top 70%',
+          end: 'bottom 0%',
+          scroller: '#camera-section-modal',
+    }})
   }, [])
   return (
     <section id='camera-section-modal' className={`${visible ? 'block top-0' : 'top-[100%]'} fixed z-[1000] right-0 w-screen h-screen flex justify-center bg-gray-300 backdrop-blur transition-all duration-500 ease-in-out pl-6 pr-6 md:pl-[25px] md:pr-3 py-16 overflow-y-scroll`}>
@@ -84,7 +126,7 @@ const CameraSectionModal = ({visible, handleClick}) => {
               </div>
             </div>
 
-            <div className="w-full flex-center mt-[100px] px-2 md:px-[50px] lg:px-0">
+            <div className="w-full flex-center mt-[100px] md px-2 md:px-[50px] lg:px-0">
               <div className="w-full md:max-w-[703.2px] lg:max-w-[980px]">
                 <img className='w-full mb-[60px]' src={cameraSectionModalImg3} alt="An image of a  dark-skined model on afro shot by the iphone 15 pro" />
                 <div className="w-full flex flex-col md:flex-row justify-center md:justify-between lg:justify-around px-8 md:px-0">
@@ -95,10 +137,12 @@ const CameraSectionModal = ({visible, handleClick}) => {
               </div>
             </div>
 
-            <div className="w-full flex-center mt-[100px] px-12 md:px-[50px] lg:px-0">
+            <div className="w-full flex-center mt-[100px] md:mt-[120px] py-[100px] md:py-[120px] px-12 md:px-[50px] lg:px-0 bg-zinc">
               <div className="w-full md:max-w-[703.2px] lg:max-w-[980px]">
-                <h3 className='typography-headline-secondary mb-[30px] lg:ml-20 max-w-[290px] lg:max-w-full'>What’s your Main mm?</h3>
-                <p id="camera-section-modal-text5" className=' text-[16px] md:text-[21px] leading-[1.381002381] font-semibold tracking-[.0113em] text-gray max-w-[280px] md:max-w-[330px] lg:max-w-[500px] mt-4 mb-10 lg:mb-[60px] lg:ml-20'>Now you can choose your default Main camera lens —<strong className='text-[#f5f5f7] font-semibold'> 24 mm, 28 mm, or 35 mm</strong>all without having to carry around extra gear. That’s the power of computational photography.</p>
+                <div className="lg:ml-28">
+                  <h3 className='typography-headline-secondary mb-[30px] max-w-[290px] lg:max-w-full'>What’s your Main mm?</h3>
+                  <p id="camera-section-modal-text5" className=' text-[16px] md:text-[21px] leading-[1.381002381] font-semibold tracking-[.0113em] text-gray max-w-[280px] md:max-w-[330px] lg:max-w-[500px] mt-4 mb-10 lg:mb-[60px]'>Now you can choose your default Main camera lens —<strong className='text-[#f5f5f7] font-semibold'> 24 mm, 28 mm, or 35 mm</strong>all without having to carry around extra gear. That’s the power of computational photography.</p>
+                </div>
                 <div className="w-full flex flex-col md:flex-row justify-center md:justify-between gap-[26px] md:gap-0">
                   <div className="">
                     <img className="w-[281px] md:w-[224px] lg:w-[310px]" src={cameraSectionModalImg4} alt="A photoshoot in 24mm of a woman" />
@@ -115,6 +159,22 @@ const CameraSectionModal = ({visible, handleClick}) => {
                 </div>
               </div>
             </div>
+
+            <div className="w-full flex-center mt-[100px] md:mt-[120px] px-12 md:px-[50px] lg:px-0">
+              <div className="w-full md:max-w-[703.2px] lg:max-w-[980px]">
+              <h3 className='typography-headline-secondary mb-[30px] max-w-[290px] lg:max-w-full'>Next‑generation portraits.</h3>
+              <div className="w-full flex flex-col md:flex-row items-center justify-center gap-[45px] md:gap-[60px] lg:gap-[80px] pt-[80px] md:py-[60px]">
+                    <div className="">
+                        <img id='camera-section-modal-image7' className='w-[275px] md:w-auto opacity-0' src={cameraSectionModalImg7} alt="An iPhone 15 Pro showing a photo of a person being taken using 2x optical zoom" />
+                    </div>
+                    <div className="typography-site-body max-w-[280px] md:max-w-[325px]">
+                        <p id='camera-section-modal-text6' className='opacity-0 translate-y-20'>The 48MP Main camera is more advanced than ever, capturing super‑high‑resolution photos with a <strong className='text-[#f5f5f7] font-semibold'>new level of detail and color</strong>.</p>
+                        <p id='camera-section-modal-text7' className='mt-4 md:mt-5 opacity-0 translate-y-20'>You’ll see the improvements in your portraits. And now you no longer have to switch to Portrait mode. If your subject is a person, dog, or cat, iPhone automatically captures depth information. So you can choose to instantly <strong className='text-[#f5f5f7] font-semibold'>see your photo as a portrait</strong>, with an artful blur effect. Or later in the Photos app.</p>
+                    </div>
+                </div>
+              </div>
+            </div>
+
         </div>
 
     </section>
